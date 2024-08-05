969caba5b3451b10fc90739e26d138c62fb5a216
= <abdoahmedsayed704@gmail.com>
2024-08-05 18:34:13 +0300

    Adding api for the project

---
+++ api/v1/views/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+index
+"""
+
+
+from flask import jsonify
+from api.v1.views import app_views
+
+
+@app_views.route('/status')
+def status():
+    return jsonify({'status': 'OK'})
