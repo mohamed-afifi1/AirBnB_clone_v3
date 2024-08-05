d80bca5b9783082ffc5034488cc935a524302693
root <root@DESKTOP-HU468P5.localdomain>
2024-08-05 20:27:24 +0300

    test

--- api/v1/app.py
+++ api/v1/app.py
@@ -28,6 +28,7 @@ def not_found(error):
     '''
     return jsonify({"error": "Not found"}), 404
 
+
 if __name__ == "__main__":
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
             port=int(os.getenv('HBNB_API_PORT', '5000')))
